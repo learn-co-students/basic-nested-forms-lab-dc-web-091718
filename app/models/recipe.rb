7068efc5d98c396a99bba555835170fa188655f7
nachoespino53 <nachoespino53@gmail.com>
2018-10-16 16:10:23 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+  before_save :erase_empty_ingredients
+
+def erase_empty_ingredients
+  # binding.pry
+  self.ingredients = self.ingredients.select do |ingredient|
+     ingredient.name != '' || ingredient.quantity != ''
+   end
+end
 end
